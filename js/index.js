cd4ab6959b5cec100f43f2e30959a86166835f17
TREY <hugh.a.lloyd@gmail.com>
2020-01-13 23:08:24 -0600

    beginning

--- js/index.js
+++ js/index.js
@@ -40,3 +40,12 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navBar = document.querySelectorAll("a");
+navBar.forEach((element,index) => element.textContent = siteContent["nav"][`nav-item-${index+1}`]);
+
+headerOne = document.querySelector("h1");
+headerOne.textContent = siteContent['cta']['h1'];
+
+headerBttn = document.querySelector('button');
+headerBttn.textContent = siteContent['cta']['button'];
